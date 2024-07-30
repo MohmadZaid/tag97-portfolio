b7ffc0674643abb4b6ec7dc12bb885f6256c3a78
zaid <mohzaid7238@gmail.com>
2024-07-30 18:55:24 +0530

    commit2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,30 @@
 import { CommonModule } from '@angular/common';
-import { Component, HostListener } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import AOS from 'aos';
+import { FeatureCardComponent } from './component/feature-card/feature-card.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { HeaderComponent } from './component/header/header.component';
-import { FeatureCardComponent } from "./component/feature-card/feature-card.component";
-import { ServiceCardComponent } from "./component/service-card/service-card.component";
-
+import { ServiceCardComponent } from './component/service-card/service-card.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, CommonModule, FeatureCardComponent, RouterLink, ServiceCardComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    CommonModule,
+    FeatureCardComponent,
+    RouterLink,
+    ServiceCardComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    AOS.init();
+  }
 
-public darkTheme = false;
-
-
+  public darkTheme = true;
 }
