99cf528f78dd8196235333c0739f31008bc40c67
zaid <mohzaid7238@gmail.com>
2024-08-01 11:47:11 +0530

    mouse effect done

--- src/app/component/custom-cursor/custom-cursor.component.ts
+++ src/app/component/custom-cursor/custom-cursor.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostListener } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 @Component({
   selector: 'tag97-custom-cursor',
   standalone: true,
@@ -7,15 +7,72 @@ import { Component, ElementRef, HostListener } from '@angular/core';
   styleUrl: './custom-cursor.component.scss',
 })
 export class CustomCursorComponent {
-  constructor(private el: ElementRef) {}
-
   @HostListener('window:mousemove', ['$event'])
   onMouseMove(event: MouseEvent) {
     const ringElement = document.getElementById('cursor');
+    const dotElement = document.getElementById('dot');
     if (ringElement) {
       ringElement.style.transform = `translate(${event.clientX - 20}px, ${
         event.clientY - 20
       }px)`;
+    }
+    if (dotElement) {
+      dotElement.style.transform = `translate(${event.clientX - 20}px, ${
+        event.clientY - 20
+      }px)`;
+    }
+  }
+
+  @HostListener('window:mouseover', ['$event'])
+  onMouseOver(event: MouseEvent) {
+    const dotElement = document.getElementById('dot');
+    const ringElement = document.getElementById('cursor');
+    const target = event.target as HTMLElement;
+
+    if (
+      target.tagName.toLowerCase() === 'a' ||
+      target.tagName.toLowerCase() === 'button'
+    ) {
+      console.log(target.tagName.toLowerCase());
+
+      if (ringElement) {
+        ringElement.classList.remove('lg:block');
+      }
+      if (dotElement) {
+        dotElement.classList.add(
+          'w-20',
+          'h-20',
+          'top-[-23px]',
+          'left-[-22px]',
+          'bg-primary/50',
+          'dark:bg-secondary/20'
+        );
+      }
+    }
+  }
+
+  @HostListener('window:mouseout', ['$event'])
+  onMouseOut(event: MouseEvent) {
+    const dotElement = document.getElementById('dot');
+    const ringElement = document.getElementById('cursor');
+    const target = event.target as HTMLElement;
+    if (
+      target.tagName.toLowerCase() === 'a' ||
+      target.tagName.toLowerCase() === 'button'
+    ) {
+      if (ringElement) {
+        ringElement.classList.add('lg:block');
+      }
+      if (dotElement) {
+        dotElement.classList.remove(
+          'w-20',
+          'h-20',
+          'top-[-23px]',
+          'left-[-22px]',
+          'bg-primary/50',
+          'dark:bg-secondary/20'
+        );
+      }
     }
   }
 }
