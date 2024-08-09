222dc5389b1c0d4be7fdc94981beccd869e201f0
zaid <mohzaid7238@gmail.com>
2024-08-09 18:58:54 +0530

    theme check user window theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -40,10 +40,21 @@ export class AppComponent implements OnInit {
     setTimeout(() => {
       this.loader = false;
     }, 1000);
-    this.darkTheme =
-      localStorage.getItem('tag97_darkTheme') == 'true' ? true : false;
+    const storedTheme = localStorage.getItem('tag97_darkTheme');
+    if (storedTheme) {
+      this.darkTheme =
+        localStorage.getItem('tag97_darkTheme') == 'true' ? true : false;
+    } else {
+      const prefersDarkScheme = window.matchMedia(
+        '(prefers-color-scheme: dark)'
+      );
+      this.darkTheme = prefersDarkScheme.matches;
+      localStorage.setItem('tag97_darkTheme', this.darkTheme.toString());
+    }
+
     AOS.init();
   }
+
   themeChange() {
     localStorage.setItem('tag97_darkTheme', this.darkTheme.toString());
   }
